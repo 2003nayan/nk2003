34e4aaf11059498e0dfd93245e024553f32c502b
2003nayan <120472927+2003nayan@users.noreply.github.com>
2023-11-08 07:25:16 +0530

    data-list.js

--- ass_2/data-list.js
+++ ass_2/data-list.js
@@ -1,8 +1,7 @@
-// Retrieve the stored registration data from Local Storage
-const registrations = JSON.parse(localStorage.getItem("registrations")) || [];  //Retrieves data from the browser's Local Storage, and associates it with the key "registrations"
+
+const registrations = JSON.parse(localStorage.getItem("registrations")) || [];  
 const userList = document.getElementById("userList");
 
-// Display the list of registration data
 registrations.forEach(userData => {
     const listItem = document.createElement("li");
     listItem.textContent = `Name: ${userData.name}, Email: ${userData.email}, Password: ${userData.password}`;
